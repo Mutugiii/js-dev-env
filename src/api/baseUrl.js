26a9cad301f28ee9f2d5156f3d2f2f6c54c40fae
Mutugi Mutuma <mutugimutuma@gmaul.com>
2019-08-31 13:09:41 +0300

    Add production build module

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,15 @@
 export default function getBaseUrl() {
-  const inDevelopment = window.location.hostname === 'localhost';     //file will look at hostname to determine if app is running in dev
-  return inDevelopment ? 'http://localhost:3001/' : '/';              //if in dev points at mockapi hosted at port 3001 & if production uses the server set up in srcServer
+  //const inDevelopment = window.location.hostname === 'localhost';     //file will look at hostname to determine if app is running in dev
+  //return inDevelopment ? 'http://localhost:3001/' : '/';              //if in dev points at mockapi hosted at port 3001 & if production uses the server set up in srcServer
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';    //same logc as above
+}
+
+function getQueryStringParameterByName(name, url) {
+  if(!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if(!results) return null;
+  if(!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ""));
 }
